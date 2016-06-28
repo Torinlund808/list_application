402594298834689fc1772f81ec534c0384c8f9cd
Torin Lund <Torinlund@icloud.com>
2016-06-28 11:56:14 -1000

    html
    html

--- js/index.js
+++ js/index.js
@@ -11,6 +11,16 @@ var cycle = function(array) {
 	}
 };
 
-var list = function() {
-	cycle(arr);
+var display = function() {
+	document.body.style.color = colors[count];
+	cycle(colors);
+
+};
+
+var listALL = function(array) {
+	for (var i = 0; i < array.length; i++) {
+		document.getElementById('colorSpace').innerHTML += "<li>" + "</
+
+	}
 }
+listAll(colors)
